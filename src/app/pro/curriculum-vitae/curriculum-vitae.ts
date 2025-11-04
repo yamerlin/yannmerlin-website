425c472b48b3a01f0f641d67c94a91b2c0c14ac0
yamerlin <merlin.yann63@gmail.com>
2025-11-04 16:29:35 +0100

    Added footer

--- src/app/pro/curriculum-vitae/curriculum-vitae.ts
+++ src/app/pro/curriculum-vitae/curriculum-vitae.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderNav } from '../header-nav/header-nav';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-curriculum-vitae',
-  imports: [HeaderNav],
+  imports: [HeaderNav, Footer],
   templateUrl: './curriculum-vitae.html',
   styleUrl: './curriculum-vitae.scss',
 })
